4601da80467294c373202553b9881ddd3d93fb9b
Shankymoti <shashankkumar648@gmail.com>
2025-01-10 18:22:57 +0530

    sdvdwv

---
+++ src/main/java/com/santos/array/Student.java
@@ -0,0 +1,46 @@
+package com.santos.array;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Student {
+    @JsonProperty("ID")
+    public String iD;
+    @JsonProperty("Name")
+    public String name;
+    @JsonProperty("Gender")
+    public String gender;
+    @JsonProperty("Class")
+    public String classaName;
+    @JsonProperty("Seat")
+    public String seat;
+    @JsonProperty("Club")
+    public String club;
+    @JsonProperty("Persona")
+    public String persona;
+    @JsonProperty("Crush")
+    public String crush;
+    @JsonProperty("BreastSize")
+    public String breastSize;
+    @JsonProperty("Strength")
+    public String strength;
+    @JsonProperty("Hairstyle")
+    public String hairstyle;
+    @JsonProperty("Color")
+    public String color;
+    @JsonProperty("Eyes")
+    public String eyes;
+    @JsonProperty("EyeType")
+    public String eyeType;
+    @JsonProperty("Stockings")
+    public String stockings;
+    @JsonProperty("Accessory")
+    public String accessory;
+    @JsonProperty("ScheduleTime")
+    public String scheduleTime;
+    @JsonProperty("ScheduleDestination")
+    public String scheduleDestination;
+    @JsonProperty("ScheduleAction")
+    public String scheduleAction;
+    @JsonProperty("Info")
+    public String info;
+}
