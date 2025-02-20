9c5981648d970b8b872e60ca2e7e09711d753828
santosh226206 <santoshamcl1996@gmail.com>
2025-02-20 22:25:35 +0530

    soting new updateg

--- src/main/java/com/santos/map/linkedHashMap/LinkedHashMapPractise.java
+++ src/main/java/com/santos/map/linkedHashMap/LinkedHashMapPractise.java
@@ -8,6 +8,9 @@ public class LinkedHashMapPractise {
         Map<String,Integer> map=new LinkedHashMap<>();
         map.put("hello",1);
         map.put("hi",2);
+        map.put("how",1);
+        map.put("are",2);
+        map.put("you",1);
         for(Map.Entry<String,Integer> item: map.entrySet()){
             System.out.println(item.getKey() + " " + item.getValue());
         }
