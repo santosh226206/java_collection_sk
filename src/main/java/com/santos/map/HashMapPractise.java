fdede444ab44be0574bbab0bc68850ce640922a6
santosh226206 <santoshamcl1996@gmail.com>
2025-02-28 21:12:44 +0530

    soting new updateg

--- src/main/java/com/santos/map/HashMapPractise.java
+++ src/main/java/com/santos/map/HashMapPractise.java
@@ -1,10 +1,18 @@
 package com.santos.map;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HashMapPractise {
     public static void main(String args[]){
         Map<Integer,String> map=new HashMap<>();
+        map.put(1,"One");
+        map.put(2,"Two");
+        map.put(3,"Three");
+        map.put(4,"Four");
+        map.put(5,"Five");
+        List<Map.Entry<Integer,String>> output=map.entrySet().stream().filter((item)->item.getKey() % 2 ==0).toList();
+        System.out.println(output);
     }
 }
