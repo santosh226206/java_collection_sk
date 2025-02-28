9222d58e9bfc4af3c2f02a1d703803221e2fe64a
santosh226206 <santoshamcl1996@gmail.com>
2025-02-28 21:05:41 +0530

    soting new updateg

--- src/main/java/com/santos/queue/src/QueueTest.java
+++ src/main/java/com/santos/queue/src/QueueTest.java
@@ -1,6 +1,7 @@
 package com.santos.queue.src;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class QueueTest {
@@ -8,8 +9,15 @@ public class QueueTest {
         Queue<String> queue=new LinkedList<>();
         //System.out.println(queue.poll());
         //queue.add("hello");
-//        System.out.println(queue.poll());
-        System.out.println(queue.remove());
+        System.out.println(queue.poll());
+        //System.out.println(queue.remove());
         System.out.println(queue);
+        queue.add("hello1");
+        queue.add("hello2");
+        queue.add("hello3");
+        queue.add("hello4");
+        queue.add("hello5");
+        List<String> output= queue.stream().filter((item)->item.equals("hello2")).toList();
+        System.out.println(output);
     }
 }
