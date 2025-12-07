28f585a2d2cf33272e38d239dea493241545d872
santosh226206 <santoshamcl1996@gmail.com>
2025-12-07 12:49:03 +0530

    soting new updateg

--- src/main/java/com/santos/iostrems/ByteArraystream.java
+++ src/main/java/com/santos/iostrems/ByteArraystream.java
@@ -6,21 +6,25 @@ import java.io.InputStream;
 public class ByteArraystream {
     public static void main(String args[]) {
 
-        byte[] array = new byte[1000];
+        byte[] array = new byte[100];
 
         try {
-            InputStream input = new FileInputStream("/media/sant-code-monk/B/job_switch/SANTOSH-KUMAR-FlowCV-Resume-20240516 (3).pdf");
+            InputStream input = new FileInputStream("/media/sant-code-monk/B/job_switch/resumedraft.txt");
 
             System.out.println("Available bytes in the file: " + input.available());
 
-            // Read byte from the input stream
-            input.read(array);
-            System.out.println("Data read from the file: ");
-
-            // Convert byte array into string
-            String data = new String(array);
-            System.out.println(data);
-
+//            // Read byte from the input stream
+//            input.read(array);
+//            System.out.println("Data read from the file: ");
+//
+//            // Convert byte array into string
+//            String data = new String(array);
+//            System.out.println(data);
+                while(input.read(array)!=-1){
+                    String data=new String(array);
+                    System.out.println("printing chunks");
+                    System.out.println(data);
+                }
             // Close the input stream
             input.close();
         } catch (Exception e) {
